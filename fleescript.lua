5687dd839117aae8ae4e0e225832374e86fb6ece
notrhiaheh <laudertoby53@gmail.com>
2026-06-30 21:52:23 +0100

    Update fleescript.lua

--- fleescript.lua
+++ fleescript.lua
@@ -49,4 +49,16 @@ Rayfield:Notify({
 })
 
 local Tab = Window:CreateTab("player", 4483362458) -- Title, Image
+local Slider = Tab:CreateSlider({
+   Name = "SpeedHack",
+   Range = {0, 48},
+   Increment = 1,
+   Suffix = "Player Speed",
+   CurrentValue = 16,
+   Flag = "Slider1", -- A flag is the identifier for the configuration file; make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+   -- The function that takes place when the slider changes
+   -- The variable (Value) is a number that correlates to the value the slider is currently at
+   end,
+})
 Rayfield:LoadConfiguration()
