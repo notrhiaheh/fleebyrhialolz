8051b4de17d6554aef1f214bc32833e916032ee2
notrhiaheh <laudertoby53@gmail.com>
2026-06-30 21:56:09 +0100

    Update fleescript.lua

--- fleescript.lua
+++ fleescript.lua
@@ -49,6 +49,20 @@ Rayfield:Notify({
 })
 
 local Tab = Window:CreateTab("player", 4483362458) -- Title, Image
+local Toggle = Tab:CreateToggle({
+   Name = "Enable Speed Hacks",
+   CurrentValue = false,
+   Flag = "Toggle1", -- A flag is the identifier for the configuration file; make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+   -- The function that takes place when the toggle is pressed
+   -- The variable (Value) is a boolean on whether the toggle is true or false
+   end,
+})
+SpeedHackEnabled:OnInputChanged(function()
+	if not SpeedHackEnabled:GetValue() and IsThereChar() then
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+	end
+end)
 local Slider = Tab:CreateSlider({
    Name = "SpeedHack",
    Range = {0, 48},
